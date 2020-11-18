9a452f1338eca6d4e43387c23a5f09b8c41d625a
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-11-18 10:45:46 -0500

    deleted part 2 min

--- dev/js/part-2-min.js
+++
@@ -1,1 +0,0 @@
-import{gsap}from"gsap";import{DrawSVGPlugin}from"gsap/DrawSVGPlugin";gsap.registerPlugin(DrawSVGPlugin);const Part2TL=gsap.timeline();Part2TL.fromTo("#blue-line",{drawSVG:"0%"},{duration:1.5,drawSVG:"100%"},"play4");export function IntroAnimation(){return Part2TL}
